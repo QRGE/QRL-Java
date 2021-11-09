8a4a3917f190169622bf7fcdafbd70dee4f411e1
QR <57096286+QRGE@users.noreply.github.com>
2021-11-09 14:30:01 +0800

    日志框架学习

--- stream/src/main/java/qrl/stream/collector/CollectorSample.java
+++ stream/src/main/java/qrl/stream/collector/CollectorSample.java
@@ -6,6 +6,7 @@ import org.qrl.util.math.NumTool;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 收集器的三大作用:
@@ -17,10 +18,33 @@ import java.util.stream.Collectors;
  */
 public class CollectorSample {
 
-    private final static List<Integer> numList = NumTool.intNumList(1, 100);
+    private final static List<Integer> numList = NumTool.intNumList(1, 10);
 
     @Test
-    public void summingInt(){
+    public void toList() {
+        List<Integer> numList = IntStream.rangeClosed(1, 10)
+                .boxed()
+                .collect(Collectors.toList());
+        for (Integer integer : numList) {
+            System.out.print(integer + " ");
+        }
+    }
+
+    /**
+     * joining() 可以把列表中的字符串连接起来
+     */
+    @Test
+    public void joining(){
+        String collect = numList.stream()
+                .map(i -> i + "").collect(Collectors.joining(", "));
+        System.out.println(collect);
+    }
+
+    /**
+     * 利用 summarizingXX 进行数据的统计
+     */
+    @Test
+    public void summarizing(){
         // statistics 是统计类, 里面包含了最大值， 最小值， 平均值，总和， 列表size
         IntSummaryStatistics statistics = numList.stream().collect(Collectors.summarizingInt(Integer::intValue));
         System.out.println("Max: " + statistics.getMax());
