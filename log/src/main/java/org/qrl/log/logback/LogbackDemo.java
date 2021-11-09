05db0c38c635658a890511bb0a89e6708c39e8c1
QR <57096286+QRGE@users.noreply.github.com>
2021-11-09 14:30:01 +0800

    日志框架学习

---
+++ log/src/main/java/org/qrl/log/logback/LogbackDemo.java
@@ -0,0 +1,18 @@
+package org.qrl.log.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author qr
+ * @date 2021/11/8 11:37 PM
+ */
+public class LogbackDemo {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LogbackDemo.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("logback.info: {}", "测试info");
+        LOGGER.error("logback.error: {}", "测试error");
+    }
+}
