aecd5130f3fd3f07a74db7d055eaa2d51e025088
QR <1826255833@qq.com>
2022-05-02 18:08:06 +0800

    插入排序的复习

--- Algorithm/src/main/java/qr/algorithm/sort/InsertionSort.java
+++ Algorithm/src/main/java/qr/algorithm/sort/InsertionSort.java
@@ -1,22 +1,36 @@
 package qr.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
- * 插入排序, 从 index=1 位置元素开始依次跟前面的元素进行比较, 选择合适的位置插入, 依次循环到 index = array.length
+ * 排序原理： 将序列分成已排序和未排序两部分，刚开始可以默认0为已排序，然后取出未排序的第一个元素插入到已排序序列的合适的位置中，重复这个过程
+ * - 插入到已排序序列中的方法可以进行优化
+ *
  * 插入排序的时间复杂度跟逆序对成正比关系
- * 最坏, 平均的时间复杂度为 O(n^2) 最好的时间复杂度为 O(n)
+ * - 最坏、平均的时间复杂度为 O(n^2)
+ * - 最好的时间复杂度为 O(n)
  * 空间复杂度为O(1)
  * 是稳定的算法
- * 和冒泡排序相比, 插入排序每次取值进行比较的序列都是有序的
+ * 和冒泡排序相比, *插入排序每次取值进行比较的序列都是有序的
+ *
+ * 想象一下打扑克时整理手牌的过程
  * @Author: QR
  * @Date: 2021/7/28-15:13
  */
 @SuppressWarnings("unused")
 public class InsertionSort<T extends Comparable<T>> extends Sort<T>{
 
+    public static void main(String[] args) {
+        Integer[] nums = {6, 9, 4, 5, 1};
+        new InsertionSort<Integer>().sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
     @Override
     protected void sort() {
         for (int begin  = 1; begin < array.length; begin++) {
             int cur = begin;
+            // 这里相当于在有序列表中冒泡插入一个元素
             while (cur > 0 && cmp(cur, cur-1) < 0){
                 swap(cur, cur-1);
                 cur--;
