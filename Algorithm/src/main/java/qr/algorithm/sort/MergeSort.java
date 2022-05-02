0ef69d775a7d97cd74d0a16f4130e3de7e7a7be0
QR <1826255833@qq.com>
2022-05-03 01:27:38 +0800

    归并排序复习及修改

--- Algorithm/src/main/java/qr/algorithm/sort/MergeSort.java
+++ Algorithm/src/main/java/qr/algorithm/sort/MergeSort.java
@@ -1,19 +1,31 @@
 package qr.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
- * 将序列不断分割成两个子序列, 分隔到不能到分隔位置(只有一个元素), 再不断成合并成一个有序序列
+ * 将序列不断分割成两个子序列，分割到不能到分割为止(只有一个元素)，将相邻的两个子序列合并成一个有序序列，重复这个过程
+ * 归并排序的时间复杂度未 O(nlogn)
+ * 但是需要额外的空间复杂度
  * @Author: QR
  * @Date: 2021/7/28-20:55
  */
-@SuppressWarnings({"unused", "unchecked"})
+@SuppressWarnings("unused")
 public class MergeSort<T extends Comparable<T>> extends Sort<T>{
 
-    private T[] leftArray;
+    public static void main(String[] args) {
+        Integer[] nums = {7, 4, 5, 8, 1, 3};
+        MergeSort<Integer> mergeSort = new MergeSort<>();
+        mergeSort.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 
+    private T[] tempArray;
+
+    @SuppressWarnings("unchecked")
     @Override
     protected void sort() {
-        leftArray = (T[]) new Comparable[array.length >> 1];
-        sort(0, array.length);
+        tempArray = (T[]) new Comparable[array.length];
+        sort(0, array.length-1);
     }
 
     /**
@@ -22,40 +34,44 @@ public class MergeSort<T extends Comparable<T>> extends Sort<T>{
      * @param end 结束索引
      */
     private void sort(int begin, int end) {
-        int restrict = 2;
-        if(end - begin < restrict) {
+        if(end <= begin) {
             return;
         }
-        int mid = (begin + end) >> 1;
+        int mid = (begin + end) / 2;
+        // 将数组分割成不可分割的序列
         sort(begin, mid);
-        sort(mid, end);
+        sort(mid+1, end);
+        // 进行排序后合并
         merge(begin, mid, end);
     }
 
     /**
-     * 将 [begin, mid) 和 [mid, end) 范围的序列合并成一个有序序列
-     * @param begin begin
-     * @param mid mid
-     * @param end end
+     * 将 [begin, mid] 和 (mid, end] 范围的序列合并成一个有序序列
      */
-    @SuppressWarnings("ALL")
     private void merge(int begin, int mid, int end) {
-        // leftStart leftEnd rightStart rightEnd
-        int ls = 0, le = mid - begin;
-        int rs = mid, re = end;
-        // 记录数组的覆盖位置, now index
-        int ni = begin;
-        // 备份左半边的数组
-        for (int i = ls; i < le; i++) {
-            leftArray[i] = array[begin + i];
-        }
-        // 左边还没结束, 即使右边数组挪完了结束了也需要将左边的数组挪过去
-        while (ls < le) {
-            if (rs < re && cmp(leftArray[ls], array[rs]) <= 0){
-                array[ni++] = leftArray[ls++];
-            }else {
-                array[ni++] = array[rs++];
+        int leftStart = begin;
+        int rightStart = mid + 1;
+        // 记录数组的覆盖位置index
+        int index = begin;
+        // 挪动数组
+        while (leftStart <= mid && rightStart <= end) {
+            if (cmp(array[leftStart], array[rightStart]) <= 0){
+                tempArray[index++] = array[leftStart++];
+            }else { // 左边数组还没挪完, 即使右边数组挪完了结束了也需要将左边的数组挪过去
+                tempArray[index++] = array[rightStart++];
             }
+        }
+        // 检测未完全挪动完的数组
+        // 左侧
+        while (leftStart <= mid) {
+            tempArray[index++] = array[leftStart++];
+        }
+        // 右侧
+        while (rightStart <= end) {
+            tempArray[index++] = array[rightStart++];
+        }
+        for (int i = begin; i <= end; i++) {
+            array[i] = tempArray[i];
         }
     }
 }
